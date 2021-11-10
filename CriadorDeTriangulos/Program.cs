e063fdef96d57dce8b98a212b0c0db34438b8869
MecaAmirati <73724736+MecaAmirati@users.noreply.github.com>
2021-11-10 05:45:06 -0300

    Add files via upload

---
+++ CriadorDeTriangulos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CriadorDeTriangulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine();
+            }*/
+
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
