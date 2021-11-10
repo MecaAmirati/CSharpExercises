d6d3b54b68bb5d2a0dbe3139aa1b1df3a496d326
MecaAmirati <73724736+MecaAmirati@users.noreply.github.com>
2021-11-10 05:21:00 -0300

    Add files via upload

---
+++ CriandoVariaveis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 2 - Criando Variaveis");
+
+            int idade;
+            idade = 28;
+
+            Console.WriteLine(idade);
+            Console.WriteLine("Sua idade é " + idade + "!");
+            
+            Console.WriteLine("Tecle  ENTER para fechar...");
+            //Console.Write("Tecle  ENTER para fechar...");//"Line" escreve E pula uma linha
+            Console.ReadLine();
+        }
+    }
+}
